6571f7232026709560d7db890ece77973d569df7
DevinAnnunzio <annunzio.da@gmail.com>
2023-10-04 14:11:10 +0900

    Feat: student DAO implementation set up

---
+++ cruddemo/src/main/java/com/annunzio/cruddemo/dao/StudentDAOImpl.java
@@ -0,0 +1,25 @@
+package com.annunzio.cruddemo.dao;
+
+import com.annunzio.cruddemo.Entity.Student;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+//Annotation for repos, supports component scanning, translates JDBC exceptions
+@Repository
+public class StudentDAOImpl implements StudentDAO{
+    //Define field for entity manager
+    private EntityManager entityManager;
+    //Inject entity manager using constructor
+    @Autowired
+    public StudentDAOImpl(EntityManager em){
+        this.entityManager = em;
+    }
+    //Implement save method
+    @Override
+    @Transactional
+    public void save(Student someStudent) {
+        entityManager.persist(someStudent);
+    }
+}
